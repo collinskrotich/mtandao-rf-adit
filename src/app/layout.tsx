e6b23dd53e8b76817ce1146b5064ec3a5b17d679
Collins Kiprono Rotich <crotich@safaricom.co.ke>
2024-02-29 03:39:24 +0300

    Removed inter font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,10 @@
 /** @format */
 
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "../lib/utils";
 import SideNavbar from "@/components/SideNavbar";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "RF Audit By Team Mtandao",
@@ -23,7 +21,6 @@ export default function RootLayout({
       <body
         className={cn(
           "min-h-screen w-full bg-white text-black flex ",
-          inter.className,
           {
             "debug-screens": process.env.NODE_ENV === "development"
           }
