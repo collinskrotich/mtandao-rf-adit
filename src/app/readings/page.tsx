f5025080795f18066f237ec6ee01daf1729bec4b
Collins Kiprono Rotich <crotich@safaricom.co.ke>
2024-03-04 03:26:31 +0300

    Added loader limegreen",

--- src/app/readings/page.tsx
+++ src/app/readings/page.tsx
@@ -1,9 +1,11 @@
 'use client';
 
-import { useEffect, useState } from "react";
+import { CSSProperties, useEffect, useState } from "react";
 import PageTitle from "@/components/PageTitle";
 import { DataTable } from "@/components/DataTable";
 import { ColumnDef } from "@tanstack/react-table";
+import ClipLoader from "react-spinners/ClipLoader";
+
 
 type Payload = {
   Longitude: number;
@@ -48,24 +50,38 @@ const columns: ColumnDef<Payload>[] = [
   },
 ];
 
-type Props = {};
+const override: CSSProperties = {
+  display: "block",
+  margin: "0 auto",
+  borderColor: "limegreen",
+};
 
+type Props = {};
 export default function UsersPage({}: Props) {
   const [payload, setPayload] = useState<Payload[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  let [color, setColor] = useState("#00FF00");
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch('https://iot-temperature-tag.vercel.app/api/rfaudit');
-      const data: Payload[] = await response.json();
+      setLoading(true); // Set loading to true when fetching data
+      try {
+        const response = await fetch('https://iot-temperature-tag.vercel.app/api/rfaudit');
+        const data: Payload[] = await response.json();
 
-      // Sort data by timestamp
-      data.sort((a: Payload, b: Payload) => new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime());
+        // Sort data by timestamp
+        data.sort((a: Payload, b: Payload) => new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime());
 
-      // Limit data to the latest 50 records
-      const latestData = data.slice(0, 200);
+        // Limit data to the latest 50 records
+        const latestData = data.slice(0, 200);
 
-      console.log("#######", latestData);
-      setPayload(latestData);
+        console.log("#######", latestData);
+        setPayload(latestData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false); // Set loading to false after data is fetched
+      }
     };
 
     // Fetch data initially
@@ -81,7 +97,22 @@ export default function UsersPage({}: Props) {
   return (
     <div className="flex flex-col gap-5  w-full">
       <PageTitle title="Latest Readings" />
-      <DataTable columns={columns} data={payload} />
+      {loading ? (
+        
+                <ClipLoader
+        color="#00FF00"
+        loading={loading}
+        cssOverride={override}
+        size={150}
+        aria-label="Loading Spinner"
+        data-testid="loader"
+      />
+        
+        
+      ) : (
+        <DataTable columns={columns} data={payload} />
+      )}
     </div>
   );
 }
+
